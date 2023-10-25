c4fd93c5c2758d0a4630803c3f4ef38dc6c3e91d
김현욱 <rlagusdnr120@gmail.com>
2023-10-25 14:58:51 +0900

    Feat: BaseEntity 생성
    created_at, updated_at, deleted_at을 공통적으로 처리할 base entity 생성

--- userservice/src/main/java/kr/co/sugarmanager/userservice/UserserviceApplication.java
+++ userservice/src/main/java/kr/co/sugarmanager/userservice/UserserviceApplication.java
@@ -3,13 +3,15 @@ package kr.co.sugarmanager.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing
 public class UserserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserserviceApplication.class, args);
+    }
 
 }
