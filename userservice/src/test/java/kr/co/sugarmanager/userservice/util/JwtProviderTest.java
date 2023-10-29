132c909655e903905f4d572c93266a2a4aa7aad5
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-10-29 12:46:50 +0900

    Test: Jwt의 만료 시간 예외 케이스 추가
    어세스토큰과 리프레쉬 모두 만료시간에 대한 예외 테스트코드 추가

--- userservice/src/test/java/kr/co/sugarmanager/userservice/util/JwtProviderTest.java
+++ userservice/src/test/java/kr/co/sugarmanager/userservice/util/JwtProviderTest.java
@@ -1,5 +1,8 @@
 package kr.co.sugarmanager.userservice.util;
 
+import kr.co.sugarmanager.userservice.exception.CustomJwtException;
+import kr.co.sugarmanager.userservice.exception.ErrorCode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,30 +14,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest(classes=JwtProvider.class)
+@SpringBootTest(classes = JwtProvider.class)
 class JwtProviderTest {
     @Value("${jwt.expired}")
     private long expired;
     @Value("${jwt.refresh-expired}")
     private long refreshExpired;
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.issuer}")
+    private String issuer;
+
     @Autowired
     private JwtProvider provider;
 
     @Nested
     @DisplayName("Create Token Test")
-    class CreateTokenTest{
+    class CreateTokenTest {
         @Nested
         @DisplayName("success")
-        class Success{
+        class Success {
 
             @Test
-            public void 어세스_토큰_생성_성공(){
-                Map<String,Object> payload = new HashMap<>();
-                String[] keys = {"key1","key2","key3"};
-                String[] values = {"value1","value2","value3"};
-                for(int i=0;i<keys.length;i++){
-                    payload.put(keys[i],values[i]);
+            public void 어세스_토큰_생성_성공() {
+                Map<String, Object> payload = new HashMap<>();
+                String[] keys = {"key1", "key2", "key3"};
+                String[] values = {"value1", "value2", "value3"};
+                for (int i = 0; i < keys.length; i++) {
+                    payload.put(keys[i], values[i]);
                 }
 
                 String token = provider.createToken(payload);
@@ -43,34 +52,84 @@ class JwtProviderTest {
                 String subject = provider.getSubject(token);
                 assertThat(subject).isEqualTo(JwtProvider.TokenType.ACCESS.getType());
 
-                for(int i=0;i<keys.length;i++){
-                    String value = provider.getClaims(token,keys[i],String.class);
+                for (int i = 0; i < keys.length; i++) {
+                    String value = provider.getClaims(token, keys[i], String.class);
                     assertThat(value).isEqualTo(values[i]);
                 }
 
-                int issuedAt = provider.getClaims(token,"iat",Integer.class);
-                int exp = provider.getClaims(token,"exp",Integer.class);
+                int issuedAt = provider.getClaims(token, "iat", Integer.class);
+                int exp = provider.getClaims(token, "exp", Integer.class);
 
-                assertThat(exp-issuedAt).isEqualTo(expired/1000);
+                assertThat(exp - issuedAt).isEqualTo(expired / 1000);
 
                 assertThat(provider.validateToken(token)).isTrue();
             }
 
             @Test
-            public void 리프레쉬_토큰_생성_성공(){
+            public void 리프레쉬_토큰_생성_성공() {
                 String refreshToken = provider.createRefreshToken();
 
                 assertThat(refreshToken).isNotBlank();
 
                 assertThat(provider.getSubject(refreshToken)).isEqualTo(JwtProvider.TokenType.REFRESH.getType());
 
-                int issuedAt = provider.getClaims(refreshToken,"iat",Integer.class);
-                int exp = provider.getClaims(refreshToken,"exp",Integer.class);
+                int issuedAt = provider.getClaims(refreshToken, "iat", Integer.class);
+                int exp = provider.getClaims(refreshToken, "exp", Integer.class);
 
-                assertThat(exp-issuedAt).isEqualTo(refreshExpired/1000);
+                assertThat(exp - issuedAt).isEqualTo(refreshExpired / 1000);
 
                 assertThat(provider.validateToken(refreshToken)).isTrue();
             }
         }
+
+        @Nested
+        @DisplayName("fail")
+        class Fail {
+            Map<String, Object> payload = new HashMap<>();
+            String[] keys = {"key1", "key2", "key3"};
+            String[] values = {"value1", "value2", "value3"};
+
+            @BeforeEach
+            public void init() {
+                //accessToken의 payload 초기화
+                for (int i = 0; i < keys.length; i++) {
+                    payload.put(keys[i], values[i]);
+                }
+            }
+
+            @Test
+            public void 어세스_토큰_만료() {
+                //accessToken의 만료시간을 0초로 조정
+                JwtProvider newProvider = new JwtProvider(0, refreshExpired, secret, issuer);
+
+                String token = newProvider.createToken(payload);
+
+                assertThat(newProvider.validateToken(token)).isFalse();
+                CustomJwtException expiredExceptionCode = assertThrows(CustomJwtException.class, () -> {
+                    newProvider.getClaims(token, keys[0], String.class);
+                });
+                assertThat(expiredExceptionCode.getErrorCode()).isEqualTo(ErrorCode.JWT_EXPIRED_EXCEPTION);
+
+                expiredExceptionCode = assertThrows(CustomJwtException.class, () -> {
+                    newProvider.getSubject(token);
+                });
+                assertThat(expiredExceptionCode.getErrorCode()).isEqualTo(ErrorCode.JWT_EXPIRED_EXCEPTION);
+            }
+
+            @Test
+            public void 리프레쉬_토큰_만료() {
+                //accessToken의 만료시간을 0초로 조정
+                JwtProvider newProvider = new JwtProvider(0, refreshExpired, secret, issuer);
+
+                String token = newProvider.createToken(payload);
+
+                assertThat(newProvider.validateToken(token)).isFalse();
+                CustomJwtException expiredExceptionCode = assertThrows(CustomJwtException.class, () -> {
+                    newProvider.getSubject(token);
+                });
+                assertThat(expiredExceptionCode.getErrorCode()).isEqualTo(ErrorCode.JWT_EXPIRED_EXCEPTION);
+            }
+        }
+
     }
 }
