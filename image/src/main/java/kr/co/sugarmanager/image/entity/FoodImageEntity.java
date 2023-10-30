d64e229e0b48eaaf965e5b61239454a5f032f172
pjy7814 <pjy7814@naver.com>
2023-10-30 17:56:45 +0900

    Feat: 이미지 저장 Entity, Repository 구현
    이미지 저장 관련 Entity와 Repository 구현하였습니다.

---
+++ image/src/main/java/kr/co/sugarmanager/image/entity/FoodImageEntity.java
@@ -0,0 +1,28 @@
+package kr.co.sugarmanager.image.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "FOOD_IMAGE")
+public class FoodImageEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "FOOD_IMAGE_PK")
+    private Long foodImagePk;
+
+    @Column(name = "MENU_PK")
+    private Long menuPk;
+
+    @Embedded
+    private ImageEntity image;
+}
---
+++ image/src/test/java/kr/co/sugarmanager/image/repository/ImageRepository.java
@@ -0,0 +1,34 @@
+package kr.co.sugarmanager.image.repository;
+
+import kr.co.sugarmanager.image.entity.FoodImageEntity;
+import kr.co.sugarmanager.image.entity.ImageEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class ImageRepository {
+    @Autowired
+    private FoodImageRepository foodImageRepository;
+
+    @Test
+    void Food_Image_저장() throws Exception {
+        //given
+        ImageEntity image = ImageEntity.builder()
+                .imageFile("cat.jpg")
+                .imagePath("food")
+                .imageUrl("https://sugarmanager.s3.ap-northeast-2.amazonaws.com/example/60339f23-116e-4a03-a75b-54a02e7ba524.jpeg")
+                .build();
+        FoodImageEntity foodImageEntity = FoodImageEntity.builder()
+                .menuPk(1L)
+                .image(image)
+                .build();
+
+        //when
+        FoodImageEntity expectedValue = foodImageRepository.save(foodImageEntity);
+
+        //then
+        assertThat(expectedValue.getFoodImagePk()).isNotNull();
+    }
+}
