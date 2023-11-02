a7a05887b7ecf02e7844a90c1a792c5db24ad3d1
pjy7814 <pjy7814@naver.com>
2023-11-02 17:26:12 +0900

    Fix: Kafka로 보내는 메시지 형식 변경

--- business/src/main/java/kr/co/sugarmanager/business/menu/dto/ImageDTO.java
+++ business/src/main/java/kr/co/sugarmanager/business/menu/dto/ImageDTO.java
@@ -1,5 +1,7 @@
 package kr.co.sugarmanager.business.menu.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,10 +10,21 @@ import java.io.Serializable;
 @Data
 @Builder
 public class ImageDTO implements Serializable {
-    private long pk;
+    private String pk;
     private ImageTypeEnum imageTypeEnum;
     private String extension;
     private long size;
     private String contentType;
     private byte[] file;
+
+    public ObjectNode getObjectNode(ObjectMapper objectMapper) {
+        ObjectNode imageInfoNode = objectMapper.createObjectNode();
+        imageInfoNode.put("pk", this.pk);
+        imageInfoNode.put("imageType", this.imageTypeEnum.name());
+        imageInfoNode.put("extension", this.extension);
+        imageInfoNode.put("size", size);
+        imageInfoNode.put("contentType", this.contentType);
+        imageInfoNode.put("file", this.file);
+        return imageInfoNode;
+    }
 }
--- business/src/main/java/kr/co/sugarmanager/business/menu/service/MenuService.java
+++ business/src/main/java/kr/co/sugarmanager/business/menu/service/MenuService.java
@@ -1,12 +1,4 @@
 package kr.co.sugarmanager.business.menu.service;
 
-import kr.co.sugarmanager.business.menu.dto.ImageDTO;
-import kr.co.sugarmanager.business.menu.dto.ImageTypeEnum;
-import org.springframework.web.multipart.MultipartFile;
-import java.util.List;
-
-
 public interface MenuService {
-    void produceMessage(Long pk, ImageTypeEnum imageTypeEnum, List<MultipartFile> multipartFile);
-    ImageDTO createImageDTO(Long pk, ImageTypeEnum imageTypeEnum, MultipartFile multipartFile);
 }
