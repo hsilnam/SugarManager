27072d033ba0da26b0b0c97cf5798c2d5a43f454
pjy7814 <pjy7814@naver.com>
2023-11-03 18:02:55 +0900

    Feat: 삭제 권한 확인을 위한 Repository 추가

--- business/src/main/java/kr/co/sugarmanager/business/menu/repository/MenuRepository.java
+++ business/src/main/java/kr/co/sugarmanager/business/menu/repository/MenuRepository.java
@@ -4,6 +4,9 @@ import kr.co.sugarmanager.business.menu.entity.MenuEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
+    Optional<MenuEntity> findByMenuPkAndUserPk(Long menuPk, Long userPk);
 }
