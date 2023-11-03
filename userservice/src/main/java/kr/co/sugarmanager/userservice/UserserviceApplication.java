1626f1d7ab2f2bd6c60570d336eb4683dbaa8c3a
김현욱 <rlagusdnr120@gmail.com>
2023-11-03 17:56:06 +0900

    Fix: Security 설정파일 수정
    - 401, 403 모두 핸들링할 수 있도록 수정

--- userservice/src/main/java/kr/co/sugarmanager/userservice/UserserviceApplication.java
+++ userservice/src/main/java/kr/co/sugarmanager/userservice/UserserviceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebSecurity
 @EnableJpaAuditing
 public class UserserviceApplication {
 
