723f9e2b719dde762a5e4445aef65faf44d72891
pjy7814 <pjy7814@naver.com>
2023-11-03 11:14:00 +0900

    Feat: 식단 저장 Service Test

---
+++ business/src/test/java/kr/co/sugarmanager/business/menu/service/MenuServiceTest.java
@@ -0,0 +1,38 @@
+package kr.co.sugarmanager.business.menu.service;
+
+import kr.co.sugarmanager.business.menu.dto.FoodDTO;
+import kr.co.sugarmanager.business.menu.dto.MenuSaveDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class MenuServiceTest {
+    @Autowired
+    private MenuService menuService;
+
+    @Test
+    void 메뉴_저장_성공() throws Exception {
+        //given
+        FoodDTO foodDTO = FoodDTO.builder()
+                .foodName("test")
+                .build();
+        List<FoodDTO> foodDTOList = new ArrayList<>();
+        foodDTOList.add(foodDTO);
+
+        MenuSaveDTO.Request request = MenuSaveDTO.Request.builder()
+                .foodList(foodDTOList)
+                .build();
+
+        //when
+        MenuSaveDTO.Response save = menuService.save(1L, null, request);
+
+        //then
+        assertEquals(save.isSuccess(), true);
+    }
+}
