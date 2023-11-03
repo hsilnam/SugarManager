f96cd6e47ac112fae85c65bc0db917ad5020ecf3
김현욱 <rlagusdnr120@gmail.com>
2023-11-03 17:56:06 +0900

    Fix: Security 설정파일 수정
    - 401, 403 모두 핸들링할 수 있도록 수정

--- userservice/src/main/java/kr/co/sugarmanager/userservice/config/SecurityConfig.java
+++ userservice/src/main/java/kr/co/sugarmanager/userservice/config/SecurityConfig.java
@@ -17,7 +17,8 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -40,30 +41,48 @@ public class SecurityConfig {
                 .httpBasic(basic -> basic.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(req ->
-                        req.requestMatchers("/api/v1/auth/**").permitAll()
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry.requestMatchers("/api/v1/auth/**").permitAll()
                                 .anyRequest().authenticated())
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exception ->
                         exception.accessDeniedHandler(new AccessDeniedHandler() {
-                            @Override
-                            public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-                                response.setStatus(HttpStatus.FORBIDDEN.value());
-                                response.setContentType(APPLICATION_JSON_VALUE);
-                                response.setCharacterEncoding("utf-8");
-                                ResponseEntity<APIUtils.ApiResult<APIUtils.ApiError>> error = APIUtils.error(new AccessDenyException(ErrorCode.FORBIDDEN_EXCEPTION));
-                                error.getHeaders().entrySet().stream().forEach(entry -> {
-                                    String key = entry.getKey();
-                                    List<String> value = entry.getValue();
-                                    value.stream().forEach(v -> {
-                                        response.setHeader(key, v);
-                                    });
-                                });
+                                    @Override
+                                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                                        response.setStatus(HttpStatus.FORBIDDEN.value());
+                                        response.setContentType(APPLICATION_JSON_VALUE);
+                                        response.setCharacterEncoding("utf-8");
+                                        ResponseEntity<APIUtils.ApiResult<APIUtils.ApiError>> error = APIUtils.error(new AccessDenyException(ErrorCode.FORBIDDEN_EXCEPTION));
+                                        error.getHeaders().entrySet().stream().forEach(entry -> {
+                                            String key = entry.getKey();
+                                            List<String> value = entry.getValue();
+                                            value.stream().forEach(v -> {
+                                                response.setHeader(key, v);
+                                            });
+                                        });
 
-                                response.getWriter().write(mapper.writeValueAsString(error.getBody()));
-                            }
-                        })
+                                        response.getWriter().write(mapper.writeValueAsString(error.getBody()));
+                                    }
+                                })
+                                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                                    @Override
+                                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                                        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                                        response.setContentType(APPLICATION_JSON_VALUE);
+                                        response.setCharacterEncoding("utf-8");
+                                        ResponseEntity<APIUtils.ApiResult<APIUtils.ApiError>> error = APIUtils.error(new AccessDenyException(ErrorCode.UNAUTHORIZATION_EXCEPTION));
+                                        error.getHeaders().entrySet().stream().forEach(entry -> {
+                                            String key = entry.getKey();
+                                            List<String> value = entry.getValue();
+                                            value.stream().forEach(v -> {
+                                                response.setHeader(key, v);
+                                            });
+                                        });
+
+                                        response.getWriter().write(mapper.writeValueAsString(error.getBody()));
+                                    }
+                                })
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
