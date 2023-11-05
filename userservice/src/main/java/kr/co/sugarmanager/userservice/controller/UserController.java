2ad61cb24f03932cb6a3ad9ea67356bb763c02e1
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-11-05 18:07:03 +0900

    Feat: Member정보 조회 api의 produces,consumes 설정
    - 둘다 APPLICATION_JSON_VALUE 로 설정

--- userservice/src/main/java/kr/co/sugarmanager/userservice/controller/UserController.java
+++ userservice/src/main/java/kr/co/sugarmanager/userservice/controller/UserController.java
@@ -9,6 +9,7 @@ import kr.co.sugarmanager.userservice.util.JwtAuthentication;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/{nickname}")
+    @GetMapping(value = "/{nickname}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ApiResult<UserInfoDTO.Response>> getMemberInfo(
             @PathVariable String nickname,
             @AuthenticationPrincipal JwtAuthentication auth
