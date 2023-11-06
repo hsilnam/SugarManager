1d874de44bcc05871b3f0d681497a4921c8b6859
pjy7814 <pjy7814@naver.com>
2023-11-06 16:52:00 +0900

    Feat: 혈당 조회 Repository 구현

--- business/src/main/java/kr/co/sugarmanager/business/bloodsugar/repository/BloodSugarRepository.java
+++ business/src/main/java/kr/co/sugarmanager/business/bloodsugar/repository/BloodSugarRepository.java
@@ -1,13 +1,17 @@
 package kr.co.sugarmanager.business.bloodsugar.repository;
 
 import kr.co.sugarmanager.business.bloodsugar.entity.BloodSugarEntity;
-import kr.co.sugarmanager.business.menu.entity.MenuEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BloodSugarRepository extends JpaRepository<BloodSugarEntity, Long> {
     Optional<BloodSugarEntity> findByBloodSugarPkAndUserPk(Long bloodSugarPk, Long userPk);
+
+    @Query("SELECT e FROM BloodSugarEntity e WHERE e.userPk = :userPk AND YEAR(e.updatedAt) = :year AND MONTH(e.updatedAt) = :month AND DAY(e.updatedAt) = :day")
+    List<BloodSugarEntity> findByUserPkAndUpdatedAt(@Param("userPk") Long userPk, @Param("year") int year, @Param("month") int month, @Param("day") int day);
+
 }
