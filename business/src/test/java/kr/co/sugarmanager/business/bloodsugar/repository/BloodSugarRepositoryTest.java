92ce210d9c0e2ed0e42dbdaecb0775e858f07b24
pjy7814 <pjy7814@naver.com>
2023-11-06 14:53:18 +0900

    Feat: 혈당 수정 Repository Test 구현

--- business/src/test/java/kr/co/sugarmanager/business/bloodsugar/repository/BloodSugarRepositoryTest.java
+++ business/src/test/java/kr/co/sugarmanager/business/bloodsugar/repository/BloodSugarRepositoryTest.java
@@ -1,13 +1,14 @@
 package kr.co.sugarmanager.business.bloodsugar.repository;
 
 import jakarta.transaction.Transactional;
-import kr.co.sugarmanager.business.bloodsugar.entity.BLOODSUGARCATEGORY;
+import kr.co.sugarmanager.business.bloodsugar.dto.BloodSugarSaveDTO;
 import kr.co.sugarmanager.business.bloodsugar.entity.BloodSugarEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class BloodSugarRepositoryTest {
@@ -20,7 +21,7 @@ public class BloodSugarRepositoryTest {
         //given
         BloodSugarEntity bloodSugarEntity = BloodSugarEntity.builder()
                 .userPk(1L)
-                .category(BLOODSUGARCATEGORY.BEFORE)
+                .category(BloodSugarSaveDTO.BLOODSUGARCATEGORY.BEFORE.name())
                 .level(200)
                 .content("test")
                 .build();
@@ -28,5 +29,47 @@ public class BloodSugarRepositoryTest {
         BloodSugarEntity save = bloodSugarRepository.save(bloodSugarEntity);
         //then
         assertThat(save.getUserPk()).isEqualTo(1L);
+    }
+
+    @Test
+    @Transactional
+    void 혈당_수정_성공() throws Exception {
+        //given
+        BloodSugarEntity bloodSugarEntity = BloodSugarEntity.builder()
+                .userPk(1L)
+                .category(BloodSugarSaveDTO.BLOODSUGARCATEGORY.BEFORE.name())
+                .level(200)
+                .content("test")
+                .build();
+        BloodSugarEntity save = bloodSugarRepository.save(bloodSugarEntity);
+
+        BloodSugarEntity bloodSugar = bloodSugarRepository.findByBloodSugarPkAndUserPk(save.getBloodSugarPk(), 1L);
+        //when
+        bloodSugar.setCategory(BloodSugarSaveDTO.BLOODSUGARCATEGORY.AFTER.name());
+        bloodSugar.setLevel(100);
+        bloodSugar.setContent("test!");
+        BloodSugarEntity expect = bloodSugarRepository.save(bloodSugar);
+
+        //then
+        assertThat(expect.getLevel()).isEqualTo(100);
+    }
+
+    @Test
+    @Transactional
+    void 혈당_수정_실패() throws Exception {
+        //given
+        BloodSugarEntity bloodSugarEntity = BloodSugarEntity.builder()
+                .userPk(1L)
+                .category(BloodSugarSaveDTO.BLOODSUGARCATEGORY.BEFORE.name())
+                .level(200)
+                .content("test")
+                .build();
+        BloodSugarEntity save = bloodSugarRepository.save(bloodSugarEntity);
+
+        //when
+        assertThrows(NullPointerException.class, () -> {
+            BloodSugarEntity bloodSugar = bloodSugarRepository.findByBloodSugarPkAndUserPk(save.getBloodSugarPk(), 2L);
+            if (bloodSugar == null) throw new NullPointerException();
+        });
     }
 }
