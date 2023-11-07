f3911a7b9781472298e6dc24234fae22e4d3ff19
배이경 <yikyung52@naver.com>
2023-11-07 17:46:28 +0900

    Feat : dto 내 에러 변수 추가

--- business/src/main/java/kr/co/sugarmanager/business/tip/dto/TipDTO.java
+++ business/src/main/java/kr/co/sugarmanager/business/tip/dto/TipDTO.java
@@ -1,5 +1,6 @@
 package kr.co.sugarmanager.business.tip.dto;
 
+import kr.co.sugarmanager.business.global.dto.ErrorResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +12,10 @@ public class TipDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Response{
+        private boolean success;
         private String title;
         private String content;
+        private String response;
+        private ErrorResponse error;
     }
 }
