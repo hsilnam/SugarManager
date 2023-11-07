5f2fb110ad47402f5803c77dc7dda05c4068d41b
김현욱 <rlagusdnr120@gmail.com>
2023-11-07 16:40:19 +0900

    Feat: AlertType에서 Enum타입을 매핑시켜주는 메서드 및 가능한 변수 범위를 string으로 return 해주는 메서드 작성

--- userservice/src/main/java/kr/co/sugarmanager/userservice/entity/AlertType.java
+++ userservice/src/main/java/kr/co/sugarmanager/userservice/entity/AlertType.java
@@ -1,5 +1,12 @@
 package kr.co.sugarmanager.userservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import kr.co.sugarmanager.userservice.exception.ErrorCode;
+import kr.co.sugarmanager.userservice.exception.ValidationException;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum AlertType {
     POKE("pokeAlert"), CHALLENGE("challengeAlert"),
     BLOOD("sugarAlert");
@@ -12,5 +19,21 @@ public enum AlertType {
 
     public String getMember() {
         return member;
+    }
+
+    @JsonCreator
+    public static AlertType getAlertType(String value) {
+        for (AlertType type : AlertType.values()) {
+            if (type.name().equals(value)) {
+                return type;
+            }
+        }
+        throw new ValidationException(ErrorCode.CATEGORY_NOT_VALID_EXCEPTION);
+    }
+
+    public static String getDomain() {
+        return Arrays.stream(AlertType.values())
+                .map(AlertType::name)
+                .collect(Collectors.joining(","));
     }
 }
