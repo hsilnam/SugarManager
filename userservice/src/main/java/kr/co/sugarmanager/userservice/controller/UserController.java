2a9533f0e504f73186a155e4614c1611bf67a365
김현욱 <rlagusdnr120@gmail.com>
2023-11-07 16:41:50 +0900

    Feat: User의 알람을 설정할 수 있는 API 구현

--- userservice/src/main/java/kr/co/sugarmanager/userservice/controller/UserController.java
+++ userservice/src/main/java/kr/co/sugarmanager/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package kr.co.sugarmanager.userservice.controller;
 
 import kr.co.sugarmanager.userservice.dto.AlarmDTO;
+import kr.co.sugarmanager.userservice.dto.AlarmUpdateDTO;
 import kr.co.sugarmanager.userservice.dto.UserInfoDTO;
 
 import static kr.co.sugarmanager.userservice.util.APIUtils.*;
@@ -56,5 +57,15 @@ public class UserController {
 
         AlarmDTO.Response res = userService.getAlarm(req);
         return result(res.isSuccess(), res, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/alarm/save", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApiResult<Object>> updateAlarm(
+            @AuthenticationPrincipal JwtAuthentication auth,
+            @RequestBody AlarmUpdateDTO.Request request
+    ) {
+        request.setUserPk(auth != null ? auth.getPk() : 0l);
+        AlarmUpdateDTO.Response response = userService.setAlarm(request);
+        return result(response.isSuccess(), response, HttpStatus.OK);
     }
 }
