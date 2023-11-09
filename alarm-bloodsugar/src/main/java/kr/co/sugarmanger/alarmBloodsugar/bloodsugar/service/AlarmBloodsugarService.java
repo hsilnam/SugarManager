1e049b5ba0ff44b8d925ebf66962d654fc02785d
배이경 <yikyung52@naver.com>
2023-11-09 15:34:42 +0900

    Revert "Merge branch 'feat/be-alarm-bloodsugar' into 'master'"
    This reverts merge request !61

--- alarm-bloodsugar/src/main/java/kr/co/sugarmanger/alarmBloodsugar/bloodsugar/service/AlarmBloodsugarService.java
+++
@@ -1,8 +0,0 @@
-package kr.co.sugarmanger.alarmBloodsugar.bloodsugar.service;
-
-
-import kr.co.sugarmanger.alarmBloodsugar.bloodsugar.dto.AlarmBloodsugarDTO;
-
-public interface AlarmBloodsugarService {
-    AlarmBloodsugarDTO.Response bloodSugarAlarms();
-}
