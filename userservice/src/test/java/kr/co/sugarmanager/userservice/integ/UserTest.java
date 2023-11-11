0fd3cda1640494cc02da41f08560d1c3639c6740
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-11-12 02:46:31 +0900

    Test: UserInfoUpdate의 name업데이트에 대한 테스트코드 완료
    - 이름 정상 수정
    - 이름 길이 초과
    - 이름 길이 부족

--- userservice/src/test/java/kr/co/sugarmanager/userservice/integ/UserTest.java
+++ userservice/src/test/java/kr/co/sugarmanager/userservice/integ/UserTest.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.co.sugarmanager.userservice.global.exception.ErrorCode;
 import kr.co.sugarmanager.userservice.global.util.JwtProvider;
+import kr.co.sugarmanager.userservice.global.util.StringUtils;
 import kr.co.sugarmanager.userservice.group.entity.GroupEntity;
 import kr.co.sugarmanager.userservice.group.repository.GroupRepository;
+import kr.co.sugarmanager.userservice.user.dto.UserInfoUpdateDTO;
 import kr.co.sugarmanager.userservice.user.entity.UserEntity;
 import kr.co.sugarmanager.userservice.user.entity.UserImageEntity;
 import kr.co.sugarmanager.userservice.user.entity.UserRoleEntity;
@@ -13,6 +15,7 @@ import kr.co.sugarmanager.userservice.user.entity.UserSettingEntity;
 import kr.co.sugarmanager.userservice.user.repository.UserRepository;
 import kr.co.sugarmanager.userservice.user.vo.RoleType;
 import kr.co.sugarmanager.userservice.user.vo.SocialType;
+import kr.co.sugarmanager.userservice.user.vo.UserInfoValidation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -33,6 +36,8 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -53,6 +58,11 @@ public class UserTest {
     private JwtProvider jwtProvider;
     @Autowired
     private ObjectMapper mapper;
+
+    private String accessToken;
+    private Map<String, Object> payload;
+    private Map<String, Object> header;
+    private UserEntity owner;
 
     private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -83,6 +93,8 @@ public class UserTest {
 
     private void assertError(ResultActions action, ErrorCode errorCode) throws Exception {
         action
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect(jsonPath("$.response", nullValue()))
                 .andExpect(jsonPath("$.error.message", is(errorCode.getMessage())))
                 .andExpect(jsonPath("$.error.code", is(errorCode.getCode())));
     }
@@ -126,16 +138,23 @@ public class UserTest {
             user.joinGroup(groupList.get(i % groupList.size()));
             groupToUser.get(groupList.get(i % groupList.size()).getGroupCode()).add(user);
         }
+
+        //login
+        owner = userList.get(0);
+        payload = new HashMap<>();
+        payload.put("id", owner.getPk());
+        payload.put("roles", owner.getRoles().stream()
+                .map(r -> r.getRole().getValue())
+                .collect(Collectors.toList()));
+        accessToken = jwtProvider.createToken(payload);
+        header = new HashMap<>();
+        header.put("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        header.put("Authorization", "Bearer ".concat(accessToken));
     }
 
     @Nested
     @DisplayName("닉네임으로 조회")
     public class FindByNickname {
-        private String accessToken;
-        private Map<String, Object> payload;
-        private Map<String, Object> header;
-        private UserEntity owner;
-
         private void assertResult(ResultActions actions, UserEntity entity) throws Exception {
             actions
                     .andExpect(jsonPath("$.response.uid", is(entity.getPk()), Long.class))
@@ -154,20 +173,6 @@ public class UserTest {
                     .andExpect(jsonPath("$.response.profileImage", is(entity.getUserImage().getImageUrl())))
                     .andExpect(jsonPath("$.response.groupCode", entity.getGroup() == null ? nullValue() :
                             is(entity.getGroup().getGroupCode())));
-        }
-
-        @BeforeEach
-        public void login() {
-            owner = userList.get(0);
-            payload = new HashMap<>();
-            payload.put("id", owner.getPk());
-            payload.put("roles", owner.getRoles().stream()
-                    .map(r -> r.getRole().getValue())
-                    .collect(Collectors.toList()));
-            accessToken = jwtProvider.createToken(payload);
-            header = new HashMap<>();
-            header.put("Content-Type", MediaType.APPLICATION_JSON_VALUE);
-            header.put("Authorization", "Bearer ".concat(accessToken));
         }
 
         @Test
@@ -199,10 +204,95 @@ public class UserTest {
             ).get(0);
 
             ResultActions action = mvc.perform(getBuilder("/api/v1/member/".concat(target.getNickname()), GET, header, null))
-                    .andExpect(status().isForbidden())
-                    .andExpect(jsonPath("$.success", is(false)))
-                    .andExpect(jsonPath("$.response", nullValue()));
+                    .andExpect(status().isForbidden());
             assertError(action, ErrorCode.FORBIDDEN_EXCEPTION);
+        }
+    }
+
+    @Nested
+    @DisplayName("업데이트")
+    public class Update {
+        private UserInfoUpdateDTO.Request req;
+        private UserInfoUpdateDTO.Request origin;
+
+        @BeforeEach
+        public void init() {
+            origin = UserInfoUpdateDTO.Request.builder()
+                    .name(owner.getName())
+                    .nickname(owner.getNickname())
+                    .bloodSugarMax(owner.getSugarMax())
+                    .bloodSugarMin(owner.getSugarMin())
+                    .birthday(owner.getBirthday())
+                    .height(owner.getHeight())
+                    .weight(owner.getWeight())
+                    .build();
+            req = UserInfoUpdateDTO.Request.builder()
+                    .name(owner.getName())
+                    .nickname(owner.getNickname())
+                    .bloodSugarMax(owner.getSugarMax())
+                    .bloodSugarMin(owner.getSugarMin())
+                    .birthday(owner.getBirthday())
+                    .height(owner.getHeight())
+                    .weight(owner.getWeight())
+                    .build();
+        }
+
+        @Nested
+        @DisplayName("이름 변경")
+        public class Name {
+            @Test
+            public void 이름_변경_성공() throws Exception {
+                req.setName("김현욱");
+                mvc.perform(getBuilder("/api/v1/member/edit", POST, header, req))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.success", is(true)))
+                        .andExpect(jsonPath("$.error", nullValue()));
+
+                UserEntity updated = userRepository.findById(owner.getPk()).get();
+                assertUpdate(updated, req);
+            }
+
+            @Test
+            public void 이름_변경_길이_부족_실패() throws Exception {
+                req.setName(StringUtils.generateRandomString(UserInfoValidation.NAME.getMin() - 1));
+                ResultActions action = mvc.perform(getBuilder("/api/v1/member/edit", POST, header, req))
+                        .andExpect(status().isBadRequest());
+                assertError(action, ErrorCode.NAME_NOT_VALID_EXCEPTION);
+                assertUpdateFail(owner);
+            }
+
+            @Test
+            public void 이름_변경_길이_초과_실패() throws Exception {
+                req.setName(StringUtils.generateRandomString(UserInfoValidation.NAME.getMax() + 1));
+                ResultActions action = mvc.perform(getBuilder("/api/v1/member/edit", POST, header, req))
+                        .andExpect(status().isBadRequest());
+                assertError(action, ErrorCode.NAME_NOT_VALID_EXCEPTION);
+                assertUpdateFail(owner);
+            }
+        }
+
+        private void assertUpdate(UserEntity updated, UserInfoUpdateDTO.Request req) {
+            assertAll("update",
+                    () -> assertThat(updated.getNickname()).isEqualTo(req.getNickname()),
+                    () -> assertThat(updated.getName()).isEqualTo(req.getName()),
+                    () -> assertThat(updated.getGender()).isEqualTo(req.getGender()),
+                    () -> assertThat(updated.getHeight()).isEqualTo(req.getHeight()),
+                    () -> assertThat(updated.getWeight()).isEqualTo(req.getWeight()),
+                    () -> assertThat(updated.getSugarMax()).isEqualTo(req.getBloodSugarMax()),
+                    () -> assertThat(updated.getSugarMin()).isEqualTo(req.getBloodSugarMin())
+            );
+        }
+
+        private void assertUpdateFail(UserEntity user) {
+            assertAll("update",
+                    () -> assertThat(user.getNickname()).isEqualTo(origin.getNickname()),
+                    () -> assertThat(user.getName()).isEqualTo(origin.getName()),
+                    () -> assertThat(user.getGender()).isEqualTo(origin.getGender()),
+                    () -> assertThat(user.getHeight()).isEqualTo(origin.getHeight()),
+                    () -> assertThat(user.getWeight()).isEqualTo(origin.getWeight()),
+                    () -> assertThat(user.getSugarMax()).isEqualTo(origin.getBloodSugarMax()),
+                    () -> assertThat(user.getSugarMin()).isEqualTo(origin.getBloodSugarMin())
+            );
         }
     }
 }
