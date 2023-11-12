1bdc6f7bd88736849807321d475b94ef45332945
Hyeon-Uk <rlagusdnr120@gmail.com>
2023-11-12 13:03:50 +0900

    Test: Weight 변경 테스트코드 추가

--- userservice/src/test/java/kr/co/sugarmanager/userservice/integ/UserTest.java
+++ userservice/src/test/java/kr/co/sugarmanager/userservice/integ/UserTest.java
@@ -348,6 +348,36 @@ public class UserTest {
             }
         }
 
+        @Nested
+        @DisplayName("weight")
+        public class Weight {
+            @Test
+            public void 체중_수정_성공() throws Exception {
+                req.setWeight(UserInfoValidation.WEIGHT.getMin() + 1);
+                mvc.perform(getBuilder("/api/v1/member/edit", POST, header, req))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.success", is(true)))
+                        .andExpect(jsonPath("$.response", nullValue()))
+                        .andExpect(jsonPath("$.error", nullValue()));
+            }
+
+            @Test
+            public void 체중_수정_실패_최소() throws Exception {
+                req.setWeight(UserInfoValidation.WEIGHT.getMin() - 1);
+                ResultActions action = mvc.perform(getBuilder("/api/v1/member/edit", POST, header, req))
+                        .andExpect(status().isBadRequest());
+                assertError(action, ErrorCode.WEIGHT_NOT_VALID_EXCEPTION);
+            }
+
+            @Test
+            public void 체중_수정_실패_최대() throws Exception {
+                req.setWeight(UserInfoValidation.WEIGHT.getMax() + 1);
+                ResultActions action = mvc.perform(getBuilder("/api/v1/member/edit", POST, header, req))
+                        .andExpect(status().isBadRequest());
+                assertError(action, ErrorCode.WEIGHT_NOT_VALID_EXCEPTION);
+            }
+        }
+
         private void assertUpdate(UserEntity updated, UserInfoUpdateDTO.Request req) {
             assertAll("update",
                     () -> assertThat(updated.getNickname()).isEqualTo(req.getNickname()),
