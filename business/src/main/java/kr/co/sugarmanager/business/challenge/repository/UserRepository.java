7a0ab4ea162bed6ddc1e1462db92a7af85c8bc04
배이경 <yikyung52@naver.com>
2023-11-13 13:07:38 +0900

    Fix : 그룹 유효성 검사, 유저 유효성 검사 추가

--- business/src/main/java/kr/co/sugarmanager/business/challenge/repository/UserRepository.java
+++ business/src/main/java/kr/co/sugarmanager/business/challenge/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    @Query("select case when coalesce(count(u.nickname),0) > 1 then true else false end as check from UserEntity u where u.pk = :userPk")
+    @Query("select case when coalesce(count(u.nickname),0) >= 1 then true else false end as check from UserEntity u where u.pk = :userPk")
     Boolean isAuthorized(@Param("userPk") Long userPk);
 
     @Query("select u.nickname from UserEntity u where u.pk = :userPk")
@@ -16,6 +16,9 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     @Query("select u.pk from UserEntity u where u.nickname = :nickname")
     Long findIdByNickname(@Param("nickname") String nickname);
 
-    @Query("select case when coalesce(count(u1.nickname),0) > 1 then true else false end as check from UserEntity u1 where u1.groupPk = (select u.groupPk from UserEntity u where u.pk = :userPk) and u1.nickname = :nickname")
+    @Query("select " +
+            "case when coalesce(count(u1.nickname),0) > 0 then true else false end as checkgroup " +
+            "from UserEntity u1 " +
+            "where u1.groupPk = (select u.groupPk from UserEntity u where u.pk = :userPk and u1.nickname = :nickname)")
     Boolean inSameGroup(@Param("userPk") Long userPk, @Param("nickname") String nickname);
 }
